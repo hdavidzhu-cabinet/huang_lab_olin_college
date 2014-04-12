5bd5ca0bea3b5213920333c460f2c95bc23f9284
hzhu <hzhu@hzhu.olin.edu>
2014-04-12 10:45:17 -0400

    I don't know.

--- blastserver.py
+++ blastserver.py
@@ -11,12 +11,21 @@ gi = 555
 database  = 'nr'
 expect_val = 10
 
-CMD = 'Web'
-PAGE = 'Proteins'
-DATABASE = 'swissprot'
-WEB = 'http://www.ncbi.nlm.nih.gov/blast/Blast.cgi?CMD='+CMD+'&\PAGE='+PAGE+'&DATABASE='+DATABASE
+# CMD = 'Web'
+# PAGE = 'Proteins'
+# DATABASE = 'swissprot'
+# WEB = 'http://www.ncbi.nlm.nih.gov/blast/Blast.cgi?CMD='+CMD+'&\PAGE='+PAGE+'&DATABASE='+DATABASE
 
-tn = telnetlib.Telnet('www.ncbi.nlm.nih.gov', 80, 60)
-tn.interact()
+tn = telnetlib.Telnet('www.ncbi.nlm.nih.gov', '80', 60)
+# tn.interact()
 tn.write('Hello')
+
+tn.write('POST /blast/Blast.cgi HTTP/1.0\n')
+tn.write('User-Agent: Hi_there\n')
+tn.write('Connection: Keep-Alive\n')
+tn.write('Content-type: application/x-www-form-urlencoded\n')
+tn.write('Content-Length: 200\n')
+    
+# CMD=Put&QUERY=555... etc
+tn.read_all()
 tn.close()
